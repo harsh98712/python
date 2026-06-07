158b826061b8ed31c539820980a6faebf791570d
harsh98712 <harshbehl8837@gmail.com>
2026-06-07 16:45:05 +0530

    behl

---
+++ chapter8_functions/funcArguments.py
@@ -0,0 +1,9 @@
+def goodday(name , ending):
+    print("Goodday, " + name )
+    print(ending)
+    return "done"
+
+
+
+a = goodday("harsh","thanks")
+print(a)
---
+++ chapter8_functions/functions.py
@@ -0,0 +1,19 @@
+# a = 5
+# b = 8
+# c = 10
+# average = (a + b+ c)/3
+# print(average)
+
+def avg():
+   a = int(input("enter the number: "))
+   b = int(input("enter the number: "))
+   c = int(input("enter the number: "))
+   average = (a + b + c)/3
+   print(average)
+
+avg()
+print("thankyou!")
+avg()
+avg()
+avg()
+avg()
